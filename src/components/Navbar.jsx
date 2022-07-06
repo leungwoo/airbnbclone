dc64da84ad3b1b3693726959e1bf6789e7ca819f
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-07-06 16:51:24 -0400

    Airbnb setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "../images/airbnblogo.png";
+
+export default function Navbar(){
+    return (
+        <div>
+            <img src={logo} alt="airbnblogo" width="70px"/>
+        </div>
+    )
+}
